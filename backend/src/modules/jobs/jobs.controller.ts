d8a78fc39f526a4c40b68710494cee19d89bb3c2
luuhaohiep1218 <luuhaohiep8a@gmail.com>
2025-06-15 15:13:37 +0700

    chinh sua job controller

--- backend/src/modules/jobs/jobs.controller.ts
+++ backend/src/modules/jobs/jobs.controller.ts
@@ -6,28 +6,33 @@ import {
   Param,
   Delete,
   Get,
+  UseGuards,
 } from "@nestjs/common";
 import { JobsService } from "./jobs.service";
 import { CreateJobDto } from "./dto/create-job.dto";
 import { UpdateJobDto } from "./dto/update-job.dto";
+import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
 @Controller("jobs")
 export class JobsController {
   constructor(private readonly jobsService: JobsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async findAll() {
     return this.jobsService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createJobDto: CreateJobDto) {
     return this.jobsService.create(createJobDto);
   }
+  @UseGuards(JwtAuthGuard)
   @Put(":id")
   update(@Param("id") id: string, @Body() updateJobDto: UpdateJobDto) {
     return this.jobsService.update(id, updateJobDto);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Delete(":id")
   delete(@Param("id") id: string) {
     return this.jobsService.delete(id);
